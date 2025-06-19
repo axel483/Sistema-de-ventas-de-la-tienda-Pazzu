71cfc2a8063ea52ee074eff5ba62967351e70ca1
AxelSacha <axelsacharomero20@gmail.com>
2025-06-19 15:07:05 -0500

    Creando la entidad Cliente

--- src/main/java/com/pazzu3/thommy/model/Cliente.java
+++ src/main/java/com/pazzu3/thommy/model/Cliente.java
@@ -1,4 +1,26 @@
 package com.pazzu3.thommy.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "clientes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombres;
+    private String apellidos;
+    private String correo;
+    private String telefono;
+    private String direccion;
 }
