e2881524d9b4d3b099de3d766714b3d93612155a
AxelSacha <axelsacharomero20@gmail.com>
2025-06-19 15:16:11 -0500

    se añadio atributos de venta y cree la entidad detalleventa

--- src/main/java/com/pazzu3/thommy/model/Venta.java
+++ src/main/java/com/pazzu3/thommy/model/Venta.java
@@ -1,5 +1,36 @@
 package com.pazzu3.thommy.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+@Entity
+@Table(name = "ventas")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Venta {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime fecha;
+
+    private Double total;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+
+    @OneToMany(mappedBy = "venta", cascade = CascadeType.ALL)
+    private List<DetalleVenta> detalles;
 }
